f411c45f5959012b9eb744e9d337d8398f247bcb
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-08 15:51:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {useState,useEffect} from 'react';
+import './App.css';
+
+
+function App() {
+  const [loading,setLoading]=useState(false);
+
+  useEffect(()=>{
+   setLoading(true) 
+setTimeout()
+  },[])
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
